db91e50425d10d7f6b209de11d091cc2065d194d
iris1230xs <791529397@qq.com>
2019-04-09 10:52:09 +0800

    level1 -> levelHard

--- scripts/level.js
+++ scripts/level.js
@@ -6,22 +6,26 @@ const intToMap = Object.freeze({
     8: 'box',
 });
 
-const level1 = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 2, 2, 2, 2, 2, 0],
-    [0, 2, 2, 2, 1, 1, 1, 2, 0],
-    [0, 2, 1, 1, 4, 9, 4, 2, 0],
-    [0, 2, 1, 1, 9, 1, 9, 2, 0],
-    [0, 2, 2, 2, 4, 9, 4, 2, 0],
-    [0, 0, 0, 2, 1, 1, 1, 2, 0],
-    [0, 0, 0, 2, 2, 2, 2, 2, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
+const levelHard = {
+    layout: [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 2, 2, 2, 2, 2, 0],
+        [0, 2, 2, 2, 1, 1, 1, 2, 0],
+        [0, 2, 1, 1, 4, 9, 4, 2, 0],
+        [0, 2, 1, 1, 9, 1, 9, 2, 0],
+        [0, 2, 2, 2, 4, 9, 4, 2, 0],
+        [0, 0, 0, 2, 1, 1, 1, 2, 0],
+        [0, 0, 0, 2, 2, 2, 2, 2, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ],
+    initRow: 3,
+    initCol: 2
+};
 
 function loadLevel(gameMap, level) {
     gameMap.clear();
-    for (let i = 0; i < level.length; i++) {
-        const row = level[i];
+    for (let i = 0; i < level.layout.length; i++) {
+        const row = level.layout[i];
         for (let j = 0; j < row.length; j++) {
             const col = row[j];
             gameMap.set(i, j, intToMap[col & 0x7]);
--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 function main() {
     const game = new Game();
-    loadLevel(game.map, level1);
     const player = new GameObject('player');
-    game.map.put(player, 3, 2);
     game.registerAction('w', () => game.map.pushUp(player));
     game.registerAction('a', () => game.map.pushLeft(player));
     game.registerAction('s', () => game.map.pushDown(player));
     game.registerAction('d', () => game.map.pushRight(player));
+    loadLevel(game.map, levelHard);
+    game.map.put(player, levelHard.initRow, levelHard.initRow);
     game.listen();
     game.start();
 }
